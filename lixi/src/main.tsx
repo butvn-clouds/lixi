b160496378ea195c5c06287be04ddf5eaa97385d
But <butvn2572@gmail.com>
2026-02-05 11:29:35 +0700

    ud

--- lixi/src/main.tsx
+++ lixi/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter  } from "react-router-dom";
 import App from "./App";
 import "./style.css";
 import "./play.css";
@@ -8,8 +8,8 @@ import "./play.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter >
       <App />
-    </BrowserRouter>
+    </HashRouter >
   </React.StrictMode>
 );
